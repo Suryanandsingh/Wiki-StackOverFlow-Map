5171a5ac07678735a31762057b722d7d40733288
Suryanand Singh <singhsuryanand@gmail.com>
2017-12-15 22:24:51 +0530

    Add files via upload

--- stackoverflow.py
+++ stackoverflow.py
@@ -4,15 +4,29 @@ import urllib.request, re
 from bs4 import BeautifulSoup
 
 pairs = [
-  ("(Show recently|Show recent|Display recent|Show) (.*)",
+  ("(show recently|show recent|Display recent|show)(.*)",
   ("{% call StackOver:%2 %}",))
 ]
+def searching(url, key, number):
+    data = urllib.parse.urlencode(key)
+    data = data.encode('utf-8')
+    request = urllib.request.Request(url, data)
+    try:
+        respounce = urllib.request.urlopen(url)
+        respdata = respounce.read()
+        soup = BeautifulSoup(respdata)
+        c = 1
+        for hr in soup.find_all("h3")[3:number + 3]:
+            print("Question No:- %s " % (c), hr.text)
+            c += 1
+    except:
+        return
 
 def StackOver(search,sessionID="general"):
     types='' #for asking  question is not answered or questions
     view = '' #for recently question
     tag=''  #tag of url
-    domain=''  #ypur domain like :- python, java etc.
+    # domain=''  #ypur domain like :- python, java etc.
     number = ''.join(x for x in search if x.isdigit()) # find number in questions
 
     if not number:
@@ -34,37 +48,21 @@ def StackOver(search,sessionID="general"):
         types='unanswered'
         tag='tab'
 
-    # tag of domain
-    if 'python' in search:
-        domain='python'
-    elif 'java' in search:
-        domain='java'
-    elif 'ace' in search:
-        domain='ace'
-
 
     # to make url of your given question
-    url = 'https://stackoverflow.com'+'/'+types+'/tagged' + '/' + domain
-
-    #key of question
-    key = {tag: view,
-                 'pageSize': 15
+    for domain in search.split(' '):
+        url = 'https://stackoverflow.com' + '/' + types + '/tagged' + '/' + domain
+        key = {tag: view,
+               'pageSize': 15
                }
+        if url == 'https://stackoverflow.com' + '/' + types + '/tagged/':
+            continue
+        searching(url, key, number)
+    else:
+        # print("else")
+        url = 'https://stackoverflow.com' + '/' + types
+        searching(url, '', number)
 
-    # to concatenate with url and values(key)
-    data = urllib.parse.urlencode(key)
-    data = data.encode('utf-8')
-    request  = urllib.request.Request(url, data)
-    respounce = urllib.request.urlopen(url)
-    respdata = respounce.read()
-
-
-    #beautifulsoup
-    soup = BeautifulSoup(respdata)
-    c=1
-    for hr in soup.find_all("h3")[3:number+3]:
-        print("Question No:- %s "%(c), hr.text)
-        c+=1
     return ''
 
 
